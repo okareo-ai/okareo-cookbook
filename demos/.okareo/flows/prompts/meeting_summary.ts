6df3fc005573dd536a1d192201666efff804a67e
Matt Wyman <matt@okareo.com>
2024-05-08 13:54:22 -0700

    Adding opnion to the summary

--- demos/.okareo/flows/prompts/meeting_summary.ts
+++ demos/.okareo/flows/prompts/meeting_summary.ts
@@ -2,12 +2,16 @@
 const USER_PROMPT_TEMPLATE = "{input}";
 
 const NUMBER_OF_WORDS = 50 - Math.round(Math.random() * 10);
+
 const EXPERT_PERSONA = `You are a City Manager with significant AI/LLM skills. 
 You are tasked with summarizing the key points from a meeting and responding in a structured manner.
 You have a strong understanding of the meeting's context and the attendees. `;
+
 const CONFUSED_PERSONA = `You are a local resident overwhelmed by the task of summarizing the key points from a meeting. 
 You have a very hard time keeping your summaries brief and will frequently write significantly more than needed. 
+You also have strong opinions about the topics and feel the need to add your opinions to the summaries even though many are not directly stated in the meeting.
 The good news is that you are comfortable with JSON format.`;
+
 const SYSTEM_MEETING_SUMMARIZER_TEMPLATE: string = `
 ${CONFUSED_PERSONA}
 
