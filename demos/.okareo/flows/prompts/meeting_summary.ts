b85c251cf99095274ca006797e748ec5f5c99433
Matt Wyman <matt@okareo.com>
2024-05-08 13:05:51 -0700

    Making the summarization persona clearer

--- demos/.okareo/flows/prompts/meeting_summary.ts
+++ demos/.okareo/flows/prompts/meeting_summary.ts
@@ -2,11 +2,16 @@
 const USER_PROMPT_TEMPLATE = "{input}";
 
 const NUMBER_OF_WORDS = 50 - Math.round(Math.random() * 10);
-const EXPERT_PERSONA = `You are a City Manager with significant AI/LLM skills. You are tasked with summarizing the key points from a meeting and responding in a structured manner.`;
-const CONFUSED_PERSONA = `You are a local resident overwhelmed by the task of summarizing the key points from a meeting. The good news is that you are comfortable with JSON format.`;
+const EXPERT_PERSONA = `You are a City Manager with significant AI/LLM skills. 
+You are tasked with summarizing the key points from a meeting and responding in a structured manner.
+You have a strong understanding of the meeting's context and the attendees. `;
+const CONFUSED_PERSONA = `You are a local resident overwhelmed by the task of summarizing the key points from a meeting. 
+You have a very hard time keeping your writing brief and will often go past requested word limits. 
+The good news is that you are comfortable with JSON format.`;
 const SYSTEM_MEETING_SUMMARIZER_TEMPLATE: string = `
 ${CONFUSED_PERSONA}
-You have a strong understanding of the meeting's context and the attendees. You are expected to provide a summary of the meeting in under ${NUMBER_OF_WORDS} words.
+
+Please provide a summary of the meeting in under ${NUMBER_OF_WORDS} words.
 Your response MUST be in the following JSON format.  Content you add should not have special characters or line breaks.
 {
     "actions": LIST_OF_TASKS_FROM_THE_MEETING,
