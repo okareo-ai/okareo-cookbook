411d48939b8a017d564effb099bbecbcb4dd0e0e
Matt Wyman <matt@okareo.com>
2024-05-08 13:01:37 -0700

    Updating summarization with a confused persona

--- demos/.okareo/flows/prompts/meeting_summary.ts
+++ demos/.okareo/flows/prompts/meeting_summary.ts
@@ -2,8 +2,10 @@
 const USER_PROMPT_TEMPLATE = "{input}";
 
 const NUMBER_OF_WORDS = 50 - Math.round(Math.random() * 10);
+const EXPERT_PERSONA = `You are a City Manager with significant AI/LLM skills. You are tasked with summarizing the key points from a meeting and responding in a structured manner.`;
+const CONFUSED_PERSONA = `You are a local resident overwhelmed by the task of summarizing the key points from a meeting. The good news is that you are comfortable with JSON format.`;
 const SYSTEM_MEETING_SUMMARIZER_TEMPLATE: string = `
-You are a City Manager with significant AI/LLM skills. You are tasked with summarizing the key points from a meeting and responding in a structured manner.
+${CONFUSED_PERSONA}
 You have a strong understanding of the meeting's context and the attendees. You are expected to provide a summary of the meeting in under ${NUMBER_OF_WORDS} words.
 Your response MUST be in the following JSON format.  Content you add should not have special characters or line breaks.
 {
