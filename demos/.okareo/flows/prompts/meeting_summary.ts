5bad57daf808e748c1b7cf2bc115c4cefa71fa76
Matt Wyman <matt@okareo.com>
2024-05-09 17:25:31 -0700

    Update to summarizer prompt

--- demos/.okareo/flows/prompts/meeting_summary.ts
+++ demos/.okareo/flows/prompts/meeting_summary.ts
@@ -3,9 +3,12 @@ const USER_PROMPT_TEMPLATE = "{input}";
 
 const NUMBER_OF_WORDS = 50 - Math.round(Math.random() * 10);
 
-const EXPERT_PERSONA = `You are a City Manager with significant AI/LLM skills. 
-You are tasked with summarizing the key points from a meeting and responding in a structured manner.
-You have a strong understanding of the meeting's context and the attendees.  You also follow rules very closely.`;
+const EXPERT_PERSONA = `
+You are a City Manager with significant AI/LLM skills. 
+You are tasked with summarizing the key points from a 
+meeting and responding in a structured manner.
+You have a strong understanding of the meeting's context 
+and the attendees.  You also follow rules very closely.`;
 
 const CONFUSED_PERSONA = `You are a local resident overwhelmed by the task of summarizing the key points from a meeting. 
 You have a very hard time keeping your summaries brief and will frequently write significantly more than needed. 
@@ -17,7 +20,7 @@ ${EXPERT_PERSONA}
 Provide a summary of the meeting in under ${NUMBER_OF_WORDS} words.
 Your response MUST be in the following JSON format.  Content you add should not have special characters or line breaks.
 {
-    "actions": LIST_OF_TASKS_FROM_THE_MEETING,
+    "actions": LIST_OF_ACTION_ITEMS_FROM_THE_MEETING,
     "short_summary": SUMMARY_OF_MEETING_IN_UNDER_${NUMBER_OF_WORDS}_WORDS,
     "attendee_list": LIST_OF_ATTENDEES
 }
