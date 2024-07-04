ab1fff972e5863b688af5617f4aa5c91a404000a
Chief Wizard <oleksii@wizardondemand.com>
2024-07-04 12:53:21 +0200

    Update Jest tests

--- tutorials/testing-ai-applications/test/tastenotes.test.tsx
+++ tutorials/testing-ai-applications/test/tastenotes.test.tsx
@@ -11,69 +11,69 @@ describe('Prompt concatenation function works', () => {
     })
 })
 
-// describe('Prompt generates a reasonable answer', async () => {
-//     const main = async () => {
-//         try {
-//             const okareo = new Okareo({api_key:OKAREO_API_KEY });
+describe('Prompt generates a reasonable answer', async () => {
+    const main = async () => {
+        try {
+            const okareo = new Okareo({api_key:OKAREO_API_KEY });
     
-//             const sData: any = await okareo.create_scenario_set({
-//                 name: "Detect Passive Intent",
-//                 project_id: project_id,
-//                 number_examples: 3,
-//                 generation_type: ScenarioType.TEXT_REVERSE_QUESTION,
-//                 seed_data: DIRECTED_INTENT
-//             });
+            const sData: any = await okareo.create_scenario_set({
+                name: "Detect Passive Intent",
+                project_id: project_id,
+                number_examples: 3,
+                generation_type: ScenarioType.TEXT_REVERSE_QUESTION,
+                seed_data: DIRECTED_INTENT
+            });
             
-//             const model_under_test = await okareo.register_model({
-//                 name: "GPT-4o with a coffee prompt v1",
-//                 tags: ["TS-SDK", "Testing"],
-//                 project_id: project_id,
-//                 models: {
-//                     type: "openai",
-//                     model_id:"gpt-4o",
-//                     temperature:0.5,
-//                     system_prompt_template: SYSTEM_PROMPT,
-//                     user_prompt_template: user_prompt
-//                 } as OpenAIModel
-//             });
+            const model_under_test = await okareo.register_model({
+                name: "GPT-4o with a coffee prompt v1",
+                tags: ["TS-SDK", "Testing"],
+                project_id: project_id,
+                models: {
+                    type: "openai",
+                    model_id:"gpt-4o",
+                    temperature:0.5,
+                    system_prompt_template: SYSTEM_PROMPT,
+                    user_prompt_template: user_prompt
+                } as OpenAIModel
+            });
     
-//             const eval_run: any = await model_under_test.run_test({
-//                 name: "TS-SDK Classification",
-//                 tags: ["Classification", "BUILD_ID"],
-//                 model_api_key: OPENAI_API_KEY,
-//                 project_id: project_id,
-//                 scenario_id: sData.scenario_id,
-//                 calculate_metrics: true,
-//                 type: TestRunType.MULTI_CLASS_CLASSIFICATION,
-//             } as RunTestProps );
+            const eval_run: any = await model_under_test.run_test({
+                name: "TS-SDK Classification",
+                tags: ["Classification", "BUILD_ID"],
+                model_api_key: OPENAI_API_KEY,
+                project_id: project_id,
+                scenario_id: sData.scenario_id,
+                calculate_metrics: true,
+                type: TestRunType.MULTI_CLASS_CLASSIFICATION,
+            } as RunTestProps );
     
-//             const reporter = new ClassificationReporter({
-//                 eval_run, 
-//                 error_max: 2, // allows for up to 2 errors 
-//                 metrics_min: {
-//                     precision: 0.95,
-//                     recall: 0.9,
-//                     f1: 0.9,
-//                     accuracy: 0.95
-//                 },
-//             });
-//             reporter.log(); // logs a table to the console output with the report results
+            const reporter = new ClassificationReporter({
+                eval_run, 
+                error_max: 2, // allows for up to 2 errors 
+                metrics_min: {
+                    precision: 0.95,
+                    recall: 0.9,
+                    f1: 0.9,
+                    accuracy: 0.95
+                },
+            });
+            reporter.log(); // logs a table to the console output with the report results
     
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     }
+        } catch (error) {
+            console.error(error);
+        }
+    }
     
-// const model_under_test = await okareo.register_model({
-//     name: "GPT-4o with a coffee prompt v1",
-//     tags: ["OpenAI", "Example"],
-//     project_id: project_id,
-//     models: {
-//         type: "openai",
-//         api_key: process.env.OPENAI_API_KEY,
-//         model_id:"gpt-4o",
-//         temperature:0.0,
-//         system_prompt_template: generate.SYSTEM_PROMPT,
-//         user_prompt_template: generate.user_prompt()
-//     } as OpenAIModel,
-// });
+const model_under_test = await okareo.register_model({
+    name: "GPT-4o with a coffee prompt v1",
+    tags: ["OpenAI", "Example"],
+    project_id: project_id,
+    models: {
+        type: "openai",
+        api_key: process.env.OPENAI_API_KEY,
+        model_id:"gpt-4o",
+        temperature:0.0,
+        system_prompt_template: generate.SYSTEM_PROMPT,
+        user_prompt_template: generate.user_prompt()
+    } as OpenAIModel,
+});
