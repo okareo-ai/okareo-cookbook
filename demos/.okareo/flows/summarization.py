3e78590a5b7fce0bbf0035d9b18805e0a2d9ce9c
hrithikd <hrithikd@berkeley.edu>
2024-09-04 14:53:11 -0700

    summarization using Haiku

--- demos/.okareo/flows/summarization.py
+++ demos/.okareo/flows/summarization.py
@@ -23,8 +23,8 @@ haiku_model_id = "claude-3-haiku-20240307"
 haiku_model_name = "Claude 3 Haiku"
 
 # Select the model to use (currently set to Haiku)
-model_id = sonnet_model_id
-model_name = sonnet_model_name
+model_id = haiku_model_id
+model_name = haiku_model_name
 
 # Define custom checks for evaluation
 custom_checks = [
