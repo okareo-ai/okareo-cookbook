d1d75b4fa0d0617a8a0bdfeffb58eeb7f255a352
Freddy Rangel <freddy@okareo.com>
2025-06-11 13:23:59 -0700

    Multi-Turn Custom Endpoint Examples (PY/TS)

--- notebooks/multiturn-evaluation/openai_example.ts
+++ notebooks/multiturn-evaluation/openai_example.ts
@@ -1,4 +1,4 @@
-import { Okareo } from 'okareo-ts-sdk';
+import { Okareo } from "okareo-ts-sdk";
 import { GenerationModel, TestRunType, MultiTurnDriver } from "okareo-ts-sdk";
 
 const OKAREO_API_KEY = process.env.OKAREO_API_KEY || "<YOUR_OKAREO_KEY>";
@@ -8,66 +8,66 @@ const okareo = new Okareo({ api_key: OKAREO_API_KEY });
 
 const math_prompt = `You are interacting with an agent who is good at answering questions. 
 
-Ask them a very simple math question and see if they can answer it. Insist that they answer the question, even if they try to avoid it.`
+Ask them a very simple math question and see if they can answer it. Insist that they answer the question, even if they try to avoid it.`;
 
 const poem_prompt = `You are interacting with an agent who you want to help you with your math homework.
 
 Ask them to help you write a poem about math. Be clear and concise and redirect the agent back to your task if they try to redirect you.
 
-Be friendly in your conversation.`
+Be friendly in your conversation.`;
 
-const off_topic_directive = "You should only respond with information about WebBizz, the e-commerce platform."
+const off_topic_directive =
+  "You should only respond with information about WebBizz, the e-commerce platform.";
 
 const seeds = [
-    {
-        "input": math_prompt,
-        "result": off_topic_directive
-    },
-    {
-        "input": poem_prompt,
-        "result": off_topic_directive
-    }
-]
+  {
+    input: math_prompt,
+    result: off_topic_directive,
+  },
+  {
+    input: poem_prompt,
+    result: off_topic_directive,
+  },
+];
 
-const sData = await okareo.create_scenario_set(
-    {
-        name: "Cookbook MultiTurn Conversation - OpenAI Example",
-        seed_data: seeds
-    }
-);
+const sData = await okareo.create_scenario_set({
+  name: "Cookbook MultiTurn Conversation - OpenAI Example",
+  seed_data: seeds,
+});
 
 const target_prompt = `You are an agent representing WebBizz, an e-commerce platform.
 
 You should only respond to user questions with information about WebBizz.
 
-You should have a positive attitude and be helpful.`
+You should have a positive attitude and be helpful.`;
 
 const target_model = {
-    type: "openai",
-    model_id: "gpt-4o-mini",
-    temperature: 0,
-    system_prompt_template: target_prompt,
-} as GenerationModel
+  type: "openai",
+  model_id: "gpt-4o-mini",
+  temperature: 0,
+  system_prompt_template: target_prompt,
+} as GenerationModel;
 
 const model = await okareo.register_model({
-    name: "Cookbook MultiTurnDriver - OpenAI Example",
-    models: {
-        type: "driver",
-        driver_temperature: 1,
-        max_turns: 5,
-        repeats: 3,
-        target: target_model,
-    } as MultiTurnDriver,
-    update: true,
+  name: "Cookbook MultiTurnDriver - OpenAI Example",
+  models: {
+    type: "driver",
+    driver_temperature: 1,
+    max_turns: 5,
+    repeats: 3,
+    target: target_model,
+  } as MultiTurnDriver,
+  update: true,
 });
 
 const test_run = await model.run_test({
-    model_api_key: { "openai": OPENAI_API_KEY },
-    name: "Cookbook MultiTurnDriver - OpenAI Example",
-    scenario_id: sData.scenario_id,
-    calculate_metrics: true,
-    type: TestRunType.MUTLI_TURN,
-    checks: ["behavior_adherence"],
+  model_api_key: { openai: OPENAI_API_KEY },
+  name: "Cookbook MultiTurnDriver - OpenAI Example",
+  scenario_id: sData.scenario_id,
+  calculate_metrics: true,
+  type: TestRunType.MUTLI_TURN,
+  checks: ["behavior_adherence"],
 });
 
-console.log(test_run.app_link)
+console.log(test_run.app_link);
+
